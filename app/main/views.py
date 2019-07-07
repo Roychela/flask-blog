035eb15865923bb42cdda764beb3f0bf88990b5f
Roy Chela <roychela@gmail.com>
2019-07-07 21:52:29 +0300

    Add all blog posts display functionality

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,18 @@
 from flask import render_template
-from . import main
+from ..models import Post
 
 @main.route('/')
 def index():
-    posts = [{'title':'abc'}, {'content':'lmn'}, {'id':'aqw'}]
-    return render_template('index.html', posts=posts)
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+
+    # Getting posts from the different categories
+    tech_posts = Post.get_posts('tech')
+    entertainment_posts = Post.get_posts('entertainment')
+    fashion_posts = Post.get_posts('fashion')
+    automobiles_posts = Post.get_posts('automobiles')
+    title = 'Home - Welcome to Blogout Online Website'
+    
+    return render_template('index.html', title = title, tech=tech_posts, entertainment=entertainment_posts, fashion=fashion_posts, automobiles=automobiles_posts)
