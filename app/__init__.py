51f0e9bc6b4dafd259a459b022c9e2a499ba08dc
Roy Chela <roychela@gmail.com>
2019-07-07 20:41:19 +0300

    Initialize flask mail extension

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask_bootstrap import Bootstrap
 from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
@@ -9,6 +10,7 @@ login_manager.login_view = 'auth.login'
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
+mail = Mail()
 
 def create_app():
     app = Flask(__name__)
@@ -16,6 +18,7 @@ def create_app():
     bootstrap.init_app(app)
     login_manager.init_app(app)
     db.init_app(app)
+    mail.init_app(app)
     #Registering main blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
