89ed37d0e521868aca0cab3b635ca949353efe54
Roy Chela <roychela@gmail.com>
2019-07-07 20:38:57 +0300

    Initialize flask db extension

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,21 @@
 from flask_bootstrap import Bootstrap
 from flask import Flask
 from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 
 bootstrap = Bootstrap()
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
     #Initializing flask extensions
     bootstrap.init_app(app)
     login_manager.init_app(app)
+    db.init_app(app)
     #Registering main blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
