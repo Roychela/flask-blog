1dd446af4e7ce967e6531fee16ee81a609bd9cbe
Roy Chela <roychela@gmail.com>
2019-07-07 20:27:19 +0300

    Add authentication blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -8,4 +8,7 @@ def create_app():
     bootstrap.init_app(app)
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+     #Registering authentication blueprint
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
     return app
