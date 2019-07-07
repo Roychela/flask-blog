f9742b154091c2bea619f6558ce0dfd7441a7405
Roy Chela <roychela@gmail.com>
2019-07-07 22:43:54 +0300

    Add user profile page view functions

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,7 @@
-from flask import render_template
-from ..models import Post
+from flask import render_template, request,redirect,url_for,abort 
+from ..models import Post, User
+from .. import db, photos
+from .forms import UpdateProfile
 
 @main.route('/')
 def index():
@@ -16,3 +18,43 @@ def index():
     title = 'Home - Welcome to Blogout Online Website'
     
     return render_template('index.html', title = title, tech=tech_posts, entertainment=entertainment_posts, fashion=fashion_posts, automobiles=automobiles_posts)
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)  
+
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html',form =form)
+
+
+@main.route('/user/<uname>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',uname=uname))    
