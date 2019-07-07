8e5c59ca68fc9e16bebcb43cc726240edd186321
Roy Chela <roychela@gmail.com>
2019-07-07 20:44:06 +0300

    Add Quote, User, Post and Comments classes

---
+++ app/models.py
@@ -0,0 +1,92 @@
+from . import db, login_manager
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+from datetime import datetime
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class Quote:
+    '''
+    Quote class to define Movie Objects
+    '''
+
+    def __init__(self,id,author,quote,permalink):
+        self.id =id
+        self.author = author
+        self.quote = quote
+        self.permalink = permalink
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255))
+    email = db.Column(db.String(255),unique = True,index = True)
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    pass_secure = db.Column(db.String(255))
+    posts = db.relationship('Post',backref = 'user',lazy = "dynamic")
+    comments = db.relationship('Comments',backref='user',lazy='dynamic')
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
+
+    def save_user(self):
+        db.session.add(self)
+        db.session.commit()
+
+
+    def __repr__(self):
+        return f'User {self.username}'
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer,primary_key = True)
+    title = db.Column(db.String(100))
+    post_content = db.Column(db.String())
+    post_category = db.Column(db.String(255))
+    posted = db.Column(db.DateTime,default=datetime.utcnow)
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+    upvotes = db.Column(db.Integer)
+    downvotes = db.Column(db.Integer)
+    comments = db.relationship('Comments',backref =  'post_id',lazy = "dynamic")
+    def save_post(self):
+        db.session.add(self)
+        db.session.commit()
+    @classmethod
+    def get_post(cls,id):
+        post = Post.query.filter_by(id=id).first()
+        return post
+
+    @classmethod
+    def get_posts(cls,post_category):
+        posts = Post.query.filter_by(post_category=post_category).all()
+        return posts
+
+class Comments(db.Model):
+    __tablename__ = 'comments'
+
+    id = db.Column(db.Integer,primary_key = True)
+    comment = db.Column(db.String())
+    post = db.Column(db.Integer,db.ForeignKey("posts.id"))
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+    
+
+    def save_comment(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_comments(cls,post):
+        comments = Comments.query.filter_by(post_id=post).all()
+        return comments        
